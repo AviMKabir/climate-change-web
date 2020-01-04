ad6fae9c857cf455603458cd7615d4e5c5e93ae8
Avi M Kabir <avimishkakabir@gmail.com>
2020-01-03 20:53:41 -0500

    changed video play functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-$(window).scroll(function() {
-    $('video').each(function() {
-        if ($(this).visible()) {
-            $(this)[0].play();
-        } else {
-            $(this)[0].pause();
-        }
-    })
-});
+// $(window).scroll(function() {
+//     $('video').each(function() {
+//         if ($(this).visible()) {
+//             $(this)[0].play();
+//         } else {
+//             $(this)[0].pause();
+//         }
+//     })
+// });
 
-// 
